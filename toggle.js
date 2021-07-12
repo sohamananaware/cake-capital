34aa13e54d20f3189766ef9705df1ef51641ebba
soham nanaware <sohamananaware@gmail.com>
2021-07-12 17:16:56 +0530

    change

---
+++ toggle.js
@@ -0,0 +1,11 @@
+var togglebtn= document.querySelector(".togglebtn")
+var closebtn= document.querySelector(".close")
+var sidebar= document.querySelector(".sidebar")
+
+
+togglebtn.addEventListener("click",()=>{
+    sidebar.classList.toggle("showsidebar")
+})
+closebtn.addEventListener("click",()=>{
+    sidebar.classList.toggle("showsidebar")
+})
